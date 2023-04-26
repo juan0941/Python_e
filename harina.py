14f5bf08f40653034762360c2f28e16335a16be1
juan andrés ruiz <111185092+juan0941@users.noreply.github.com>
2023-04-26 08:09:14 -0500

    Delete harina.py

--- harina.py
+++
@@ -1,6 +0,0 @@
-num=int(input("ingrese la cantidad de kilos de harina que desea"))
-kilo=500
-if num>10:
-    kilo=kilo-100
-precio=kilo*num
-print("el valor total de ",num," kilos de harina es: ",precio)
