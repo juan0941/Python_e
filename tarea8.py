73a59483178cc0d2a0c504f5e4db2e0a92307a33
juan andrés ruiz <111185092+juan0941@users.noreply.github.com>
2023-04-26 08:09:51 -0500

    Delete tarea8.py

--- tarea8.py
+++
@@ -1,6 +0,0 @@
-nom = str(input("ingrese su nombre: "))
-edad = int(input("ingrese su edad: "))
-if edad >= 18:
-    print(nom, "usted es mayor de edad")
-else:
-    print(nom, "usted es menor de edad")
